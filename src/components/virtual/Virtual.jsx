cc48685c595bdc1e5d5a750553e772b3a06c0c1d
kirtiraj22 <kirtirajpatanwadia064@gmail.com>
2023-05-17 12:05:12 +0530

    added left portion of virtual component

--- src/components/virtual/Virtual.jsx
+++ src/components/virtual/Virtual.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
-
+import css from './Virtual.module.css';
+import Shade from '../../assets/shade.png';
 const Virtual = () => {
-  return <div>Virtual</div>;
+  return (
+    <div className={css.Virtual}>
+      <div className={css.left}>
+        <span>Virtual Try-on</span>
+        <span>Never Buy the wrong shade Again!</span>
+        <span>Try Now!</span>
+        <img
+          src={Shade}
+          alt=''
+        />
+      </div>
+      <div className={css.right}>Virtual Component</div>
+    </div>
+  );
 };
 
 export default Virtual;
