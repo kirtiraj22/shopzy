d574617b945d255c54edc0801c182b5202a8e070
kirtiraj22 <kirtirajpatanwadia064@gmail.com>
2023-05-18 17:35:38 +0530

    added testimonial section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/header/Header';
 import Hero from './components/hero/Hero';
 import Products from './components/products/Products';
 import Slider from './components/slider/Slider';
+import Testimonials from './components/testimonials/Testimonials';
 import Virtual from './components/virtual/Virtual';
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Slider />
       <Virtual />
       <Products />
+      <Testimonials />
     </div>
   );
 }
