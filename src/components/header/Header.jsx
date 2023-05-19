ff71a111ceb852ed377a921772ad151ab8f7c730
kirtiraj22 <kirtirajpatanwadia064@gmail.com>
2023-05-19 14:00:44 +0530

    added responsiveness for tablet

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import css from './Header.module.css';
 import Logo from '../../assets/logo.png';
 import { CgShoppingBag } from 'react-icons/cg';
+import { GoThreeBars } from 'react-icons/go';
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(true);
+  const toggleMenu = () => {
+    setShowMenu((showMenu) => !showMenu);
+  };
   return (
     <div className={css.container}>
       <div className={css.logo}>
-        <img src={Logo} alt='logo' />
+        <img
+          src={Logo}
+          alt='logo'
+        />
         <span>Shopzy</span>
       </div>
       <div className={css.right}>
-        <div className={css.menu}>
-          <ul className={css.menu}>
-            <li>Collections</li>
-            <li>Brands</li>
-            <li>new</li>
-            <li>Sales</li>
-            <li>ENG</li>
-          </ul>
+        <div
+          className={css.bars}
+          onClick={toggleMenu}
+        >
+          <GoThreeBars />
         </div>
-        <input type='text' placeholder='Search' className={css.search} />
+        <ul
+          className={css.menu}
+          style={{
+            display: showMenu ? 'inherit' : 'none'
+          }}
+        >
+          <li>Collections</li>
+          <li>Brands</li>
+          <li>new</li>
+          <li>Sales</li>
+          <li>ENG</li>
+        </ul>
+        <input
+          type='text'
+          placeholder='Search'
+          className={css.search}
+        />
         <CgShoppingBag className={css.cart} />
       </div>
     </div>
